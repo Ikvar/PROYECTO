d10bb242f50d6e6a53d62849dc9c59b4cd8123f6
Miguel Blanco <miguel.blanco.miranda@gmail.com>
2018-06-08 19:35:39 +0200

    Cambios

--- app/src/main/java/com/example/dev2/proyecto/Cartas.java
+++ app/src/main/java/com/example/dev2/proyecto/Cartas.java
@@ -2,21 +2,22 @@ package com.example.dev2.proyecto;
 
 public class Cartas {
 
-    private String nombre, coste, tipo, edicion, texto;
-    private int fuerza, resistencia;
+    private String nombre;
+    private String coste;
+    private String tipo;
+    private String edicion;
+    private String texto;
+    private int fuerza;
+    private int resistencia;
 
-    public Cartas(String nombre, String coste, String tipo, String edicion, int i, int fuerza, String s){
-
-    }
-
-    public Cartas(String nombre, String coste, String tipo, String edicion, String texto, int fuerza, int resistencia){
+    public Cartas(String nombre, String coste, String tipo, String texto, int fuerza, int resistencia, String edicion){
         this.nombre = nombre;
         this.coste = coste;
         this.tipo = tipo;
-        this.edicion = edicion;
         this.texto = texto;
         this.fuerza = fuerza;
         this.resistencia = resistencia;
+        this.edicion = edicion;
     }
 
     public String getNombre() {
